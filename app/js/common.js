b4f0690c7802ac4f3661dc18449ebbd89af9e5bd
Sav4enkoStanislav <savchenko.stanislav.s@gmail.com>
2018-08-28 21:47:38 +0300

    Me need drink

---
+++ app/js/common.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        items:1,
+        autoplay: true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:false
+    })});
